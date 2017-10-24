c8ff54d3152133b3e0a7b190a0d4986ac84f4aa6
Janus Nicon <janusnic@gmail.com>
2017-10-24 12:50:27 +0300

    added unit 3

--- ex1.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs');
-let content;
-
-try {
-  content = fs.readFileSync('README.md', 'utf-8');
-} catch (ex) {
-  console.log(ex);
-}
-console.log(content);
--- ex2.js
+++
@@ -1,8 +0,0 @@
-const fs = require('fs');
-fs.readFile('README.md', 'utf-8', function (err, content) {
-  if (err) {
-    return console.log(err);
-  }
-
-  console.log(content);
-});
